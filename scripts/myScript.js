a015465e1c9e82a921b8862945f2795788c20aeb
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-10-07 13:45:25 -0700

    first commit

---
+++ scripts/myScript.js
@@ -0,0 +1,26 @@
+(function(){
+
+//CREATING HOTEL OBJECT AND WRITING OUT OFFER DETAILS
+
+var hotel = {
+    name: 'Park',
+    roomRate: 240,
+    discount: 15,
+    offerPrice: function(){
+        var offerRate = this.roomRate * ((100 - this.discount) / 100);
+        return offerRate;
+    }
+};
+
+var hotelName, roomRate, specialRate;
+hotelName = document.getElementById("hotelName");
+roomRate = document.getElementById("roomRate");
+specialRate = document.getElementById("specialRate");
+
+hotelName.textContent = hotel.name;
+roomRate.textContent = "$" + hotel.roomRate.toFixed(2);
+specialRate.textContent = "$" + hotel.offerPrice();
+console.log("I am here");
+
+
+})();
