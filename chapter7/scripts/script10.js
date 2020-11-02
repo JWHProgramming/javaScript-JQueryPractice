dc2e00792b62137cdc7d62fd43fa48d1775d82e3
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-11-02 12:57:36 -0800

    Changes made notes

--- chapter7/scripts/script10.js
+++ chapter7/scripts/script10.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     //Practicing the event object and its methods and properties
+    // Single note :  e.timeStamp does not work properly due to changes made to DOM by creators
     $('li').on('click', function(e){
         $('li span').remove();
         var date = new Date();
