20e6ee06ee36060866c7f09dbdab6a58b45776d8
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-11-02 13:10:11 -0800

    Basic effects like toggle and hide

--- chapter7/scripts/script10.js
+++ chapter7/scripts/script10.js
@@ -7,4 +7,30 @@ $(document).ready(function(){
         var clicked = date.toDateString();
         $(this).append('<span class=date"> ' + clicked + ' ' + e.type + '</span>');
     });
+
+    // Delegating Events using additional parameters for event handlers
+    // .on(events,[selector],[data], function(e))
+
+    var listItem, itemStatus, eventType;
+
+    $('ul').on('mouseover',
+    ':not(#four)',
+    {status: 'important'},
+    function(e){
+        listItem = 'Item: ' + e.target.textContent + '<br />';
+        itemStatus = 'Status: ' + e.data.status + '<br />';
+        eventType = 'Event: ' + e.type;
+        $('#notes').html(listItem + itemStatus + eventType);
+    })
+
+    // Basic effects
+
+    $('h2').hide().slideDown();
+    var $li = $('li');
+    $li.hide().each(function(index){
+        $(this).delay(700*index).fadeIn(700);
+    });
+    $li.on('click', function(){
+        $(this).fadeOut(700);
+    });
 })
