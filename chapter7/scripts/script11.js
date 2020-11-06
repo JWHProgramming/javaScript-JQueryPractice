1b0ff8dae6d4e27cfa77448e1e16d04ff9e3a4c5
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-11-06 10:51:38 -0800

    Filters

--- chapter7/scripts/script11.js
+++ chapter7/scripts/script11.js
@@ -4,7 +4,22 @@ $(document).ready(function(){
     $h2.append('<a class="show">show</a>');
 
     $h2.on('click', function(){
-        $h2.next().fadeIn(500).children('.hot').addClass('complete');
+        $h2.next().fadeIn(500);
         $h2.find('a').fadeOut();
     })
+
+    var $listItems = $('li');
+    $listItems.filter('.hot:last').removeClass('hot');
+    $('li:not(.hot)').addClass('cool');
+    $('li:first').removeClass('cool');
+    $listItems.has('em').addClass('complete');
+
+    $listItems.each(function(){
+        var $this = $(this);
+        if($this.is('.hot')){
+            $this.prepend('Priority item: ');
+        }
+    })
+
+    $('li:contains("Honey")').append('Local');
 })
