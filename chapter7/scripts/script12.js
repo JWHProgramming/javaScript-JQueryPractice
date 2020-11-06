14839bab2a4d60903767f1df7a2aa9ba0abc814a
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-11-06 12:31:29 -0800

    Working with forms

---
+++ chapter7/scripts/script12.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var $newItemButton = $('#newItemButton');
+    var $newItemForm = $('#newItemForm');
+    var $textInput = $('input:text');
+
+    $newItemButton.show()
+    $newItemForm.hide();
+
+    $newItemButton.on('click', function(){
+        $newItemButton.hide();
+        $newItemForm.show();
+    });
+
+    $newItemForm.on('submit', function(e){
+        e.preventDefault();
+        var newText = $textInput.val();
+        $('li:last').after('<li>' + newText + '</li>');
+        $newItemForm.hide();
+        $newItemButton.show();
+        $textInput.val('');
+    })
+    
+})
