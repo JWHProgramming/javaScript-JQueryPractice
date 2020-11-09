0fffb1025c8e1a344250910d65b54f8ccc08ea79
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-11-08 16:18:36 -0800

    Finished small project 13

--- chapter7/scripts/script13.js
+++ chapter7/scripts/script13.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
     var $window = $(window);
     var $slideAd = $("#slideAd");
-    var endZone = $("#footer").offset().top - $window.height() - 500;
+    var endZone = $("#footer").offset().top - $window.height()-500;
 
     $window.on('scroll', function(){
+        endZone = $("#footer").offset().top - $window.height()-500;
         if( (endZone) < $window.scrollTop()){
+            console.log("here");
             $slideAd.animate({
                 'right':'0px'
-            }, 250);
+            }, 1000);
         }else{
+            console.log("not here");
             $slideAd.stop(true).animate({
                 'right':'-360px'
-            },250);
+            },1000 );
         }
     })
 })
