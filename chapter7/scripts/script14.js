11e39d2688b3882fb68b0d6de0746a572245a8ba
Jeffrey Hendricks <jwhprogramming@gmail.com>
2020-11-08 16:38:32 -0800

    Delay and fadeIn functions setup

--- chapter7/scripts/script14.js
+++ chapter7/scripts/script14.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+// SETUP
+
+var $list, $newItemForm, $newItemButton;
+var item = '';
+
+$list = $('ul');
+$newItemForm = $('#newItemForm');
+$newItemButton = $('newItemButton');
+
+$('li').hide().each(function(index){
+    $(this).delay(450 * index).fadeIn(1600);
+});
 
 })
